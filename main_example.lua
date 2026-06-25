fb6f83f730049e7c0555b941c19ae5de2a9e439d
mmdcoding <helper.bestofkills@gmail.com>
2026-06-25 16:15:05 +0330

    Create main_example.lua

---
+++ main_example.lua
@@ -0,0 +1,8 @@
+--[[
+    WindUI Example 
+
+    example moved to main.client.lua
+]]
+
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/mmdcoding/Script-GUi/refs/heads/main/main.client.lua'))()
